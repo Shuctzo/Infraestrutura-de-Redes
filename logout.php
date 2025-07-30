135634f362bd4e7d9e6e82f692998ad460a850a5
Shuctzo <matheusshuctzo@gmail.com>
2025-07-30 02:38:35 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// public/admin/logout.php
-session_start();
-session_destroy(); // Destrói todas as variáveis de sessão
-header('Location: index.php'); // Redireciona para a página de login
-exit();
-?>
